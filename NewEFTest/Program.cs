13c7a03f4ed602330b0c8f97230e22ed3abd6fa7
Ren12Bol <renat13bolurov@gmail.com>
2022-01-22 17:52:43 +0300

    ksdlgksg

--- NewEFTest/Program.cs
+++ NewEFTest/Program.cs
@@ -15,27 +15,38 @@ namespace NewEFTest
 
         public class ExcelManager
         {
+            //Создание экземпляра программы
             Application app;
+            //Создание экземпляра книги
             Workbook wb;
+            //Создание экземпляра листа
             Worksheet ws;
-            Range rg;
             string _pathAndName;
 
+
+            //Это метод для непосредственно создания файла
             public ExcelManager(string pathAndName)
             {
                 _pathAndName = pathAndName;
                 app = new Application();
+
+                /* Workbooks.Add cоздаёт книгу
+                 * XlWBATemplate.xlWBATWorksheet это тип книги
+                 * Не обязателен, но лучше пусть будет */
                 wb = app.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+                
+                //Открывает для программы первый лист книги
                 ws = (Worksheet)wb.Worksheets[1];
                 ws.Name = "Data";
-                rg = ws.UsedRange;
             }
 
+            // Метод для заполнения листа
             public void FillCell(int row, int column, string value)
             {
                 ws.Cells[row, column] = value;
             }
 
+            // Метод для сохранения файла
             public void Save()
             {
                 wb.SaveAs(_pathAndName);
@@ -47,7 +58,9 @@ namespace NewEFTest
         {
             Console.WriteLine("Путь, по которому должен быть сохранён файл:");
             Console.WriteLine(Path.Combine(Environment.CurrentDirectory));
-            ExcelManager em = new ExcelManager(Path.Combine(Environment.CurrentDirectory));
+
+            //В скобках страшное это текущий путь
+            ExcelManager em = new ExcelManager(Path.Combine(Environment.CurrentDirectory) + @"\NameOfTheFile.xlsx");
             em.FillCell(1, 2, "Man");
             em.Save();
 
